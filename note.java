33141897cbe1a3f6de9ff0df3d86e9c34b7e526a
liqiang <qiangli_i697@anjuke.com>
2016-08-01 16:06:15 +0800

    tri001

---
+++ note.java
@@ -0,0 +1,13 @@
+public class House(
+	private Integer houseId;
+	private String address;
+	private int test;
+	private int tri001;
+	
+	public String getAddress(){
+		return address;
+	}
+	public void getTest1(){
+		return test1;
+	}
+)
